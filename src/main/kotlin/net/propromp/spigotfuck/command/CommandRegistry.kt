9e54db2e8350bae0fa57fceaeffc8699d08cfe60
Propromp <p@propromp.net>
2022-02-20 20:18:18 +0900

    async

--- src/main/kotlin/net/propromp/spigotfuck/command/CommandRegistry.kt
+++ src/main/kotlin/net/propromp/spigotfuck/command/CommandRegistry.kt
@@ -5,9 +5,10 @@ import net.propromp.neocommander.api.CommandManager
 import net.propromp.neocommander.api.argument.StringArgument
 import net.propromp.neocommander.api.builder.CommandBuilder
 import net.propromp.spigotfuck.io.CommandSenderIOPipe
+import org.bukkit.Bukkit
 import org.bukkit.plugin.Plugin
 
-class CommandRegistry(plugin:Plugin) {
+class CommandRegistry(val plugin:Plugin) {
     val commandManager = CommandManager(plugin)
     fun load() {
         commandManager.register(
@@ -15,14 +16,16 @@ class CommandRegistry(plugin:Plugin) {
                 .arguments(StringArgument("brainfuck"))
                 .executes {
                     val brainfuck = it.getArgument("brainfuck",String::class.java)
-                    it.source.sendMessage("brainfuckを実行します")
-                    val pipe = CommandSenderIOPipe.getOrCreate(it.source.sender)
-                    val interpreter = BrainfuckInterpreter(brainfuck, pipe)
-                    val mill = System.currentTimeMillis()
-                    interpreter.execute()
-                    it.source.sendMessage(pipe.str)
-                    pipe.str = ""
-                    it.source.sendMessage("${System.currentTimeMillis()-mill}msで実行しました。")
+                    Bukkit.getScheduler().runTaskAsynchronously(plugin, Runnable {
+                        it.source.sendMessage("brainfuckを実行します")
+                        val pipe = CommandSenderIOPipe.getOrCreate(it.source.sender)
+                        val interpreter = BrainfuckInterpreter(brainfuck, pipe)
+                        val mill = System.currentTimeMillis()
+                        interpreter.execute()
+                        it.source.sendMessage(pipe.str)
+                        pipe.str = ""
+                        it.source.sendMessage("${System.currentTimeMillis()-mill}msで実行しました。")
+                    })
                 }.build()
         )
     }
